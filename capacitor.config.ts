b8392518df57ead28a2eacf1da68907c36fce708
TomasitoProgram <to.duhalde@duocuc.cl>
2024-09-22 00:09:10 -0300

    base git hub

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ContraseñaApp',
+  webDir: 'www'
+};
+
+export default config;
